72035122c329addc9c5884c53f07647870bd4951
Him188 <Him188@mamoe.net>
2022-11-19 01:46:06 +0000

    update

--- ICReactor/test.lua
+++ ICReactor/test.lua
@@ -6,3 +6,12 @@ coroutine.wrap(function()
         wait(1)
     end
 end)()
+
+function wait(t)
+    t = t or 0 -- default is 0 seconds
+    local start = tick()
+    local stop = start + t
+    repeat coroutine.yield() until tick() > stop
+    -- Return the time we waited + that second thingy of wait
+    return tick() - start, tick() -- #2 isn't  tick(), but nobody knows
+end
